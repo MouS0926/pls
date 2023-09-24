3e8234d6e07c22df933662d9b8ab6673526b526c
MouS0926 <moupriya2609@gmail.com>
2023-09-25 01:10:34 +0530

    t

---
+++ index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const {connection}=require("./db")
+// const {UserRouter} =require("./routes/userrouter")
+
+const cors=require("cors")
+
+
+
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+// app.use("/user",userRouter)
+
+app.get("/",(req,res)=>{
+    res.status(200).send("get req")
+})
+
+app.listen(8080,async ()=>{
+    
+    try {
+        await connection
+        console.log("db is connected");
+        console.log("server is running");
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
